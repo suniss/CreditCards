522d11c4e8676c6437e373960f30905e15a59721
suniss <sunisa.kancha@outlook.com>
2022-12-27 13:49:37 +0100

    Smoke Test

--- CreditCard.UITests/CreditCardWebAppShould.cs
+++ CreditCard.UITests/CreditCardWebAppShould.cs
@@ -7,15 +7,39 @@ namespace CreditCard.UITests
 {
     public class CreditCardWebAppShould
     {
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string HomeTitle = "Home Page - Credit Cards";
         [Fact]
         [Trait("Category", "Smoke")]
         public void LoadApplicatuibPage()
         {
             using (IWebDriver driver = new ChromeDriver()) 
             {
-                driver.Navigate().GoToUrl("http://localhost:44108/");
+              
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                TestHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
             }
 
+        }
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver()) 
+            {
+               
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause();
+                driver.Navigate().Refresh();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
         }
     }
 }
