82747897cd2b296d76e4ce83c3a85d7038712022
suniss <sunisa.kancha@outlook.com>
2022-12-27 14:38:20 +0100

    Added Back and Forward reload

--- CreditCard.UITests/CreditCardWebAppShould.cs
+++ CreditCard.UITests/CreditCardWebAppShould.cs
@@ -8,6 +8,7 @@ namespace CreditCard.UITests
     public class CreditCardWebAppShould
     {
         private const string HomeUrl = "http://localhost:44108/";
+        private const string AboutUrl = "http://localhost:44108/Home/About";
         private const string HomeTitle = "Home Page - Credit Cards";
         [Fact]
         [Trait("Category", "Smoke")]
@@ -32,7 +33,6 @@ namespace CreditCard.UITests
             using (IWebDriver driver = new ChromeDriver()) 
             {
                
-
                 driver.Navigate().GoToUrl(HomeUrl);
                 TestHelper.Pause();
                 driver.Navigate().Refresh();
@@ -41,5 +41,47 @@ namespace CreditCard.UITests
                 Assert.Equal(HomeUrl, driver.Url);
             }
         }
+
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void GetHomePage() 
+        { 
+            using(IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause();
+                driver.Navigate().GoToUrl(AboutUrl);
+                TestHelper.Pause();
+                driver.Navigate().Back();
+                TestHelper.Pause();
+
+                Assert.Equal(HomeTitle, driver.Title);  
+                Assert.Equal(HomeUrl, driver.Url);
+
+                // TODO: assert that page was reloaded
+            }
+        }
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnForward() 
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause();
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause();
+
+                driver.Navigate().Forward();
+                TestHelper.Pause(); 
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                //TODO: assert that page was reloaded
+            }
+        }
+            
     }
 }
